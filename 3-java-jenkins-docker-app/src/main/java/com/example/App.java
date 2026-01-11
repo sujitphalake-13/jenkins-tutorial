0881e9acca95edb249e5673527b91f08afb13fa2
Sujit Phalake <sujit.phalake13@gmail.com>
2026-01-11 17:42:29 +0530

    Update App.java

--- 3-java-jenkins-docker-app/src/main/java/com/example/App.java
+++ 3-java-jenkins-docker-app/src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Java Application for Jenkins CI/CD!");
+        System.out.println("Hello from Java Application for Jenkins CI-CD!");
     }
 }
